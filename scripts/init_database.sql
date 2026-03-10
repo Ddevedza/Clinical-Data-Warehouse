de7d70db742db64d58da4522cf6335376a96d568
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-10 17:46:04 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,13 +4,13 @@ Create Database and Schemas
 ====================================
 Script Purpose:
 
-	This script creates a new database named 'Covid19_DataWarehouse' after checking if it already exists.
+	This script creates a new database named 'Clynical_DataWarehouse' after checking if it already exists.
 	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
 	within the database: 'bronze', 'silver', and 'gold'.
 
 WARNING:
 
-	Running this entire script will drop the entire 'Covid19_DataWarehouse' database if it exists.
+	Running this entire script will drop the entire 'Clynical_DataWarehouse' database if it exists.
 	All data in the database will be permanently deleted. Proceed with caution
 	and ensure you have proper backups before running this script.
 */
@@ -18,19 +18,19 @@ WARNING:
 USE master;
 GO
 
--- Drop and recreate the 'Covid19_DataWarehouse' database
-IF EXISTS(SELECT 1 FROM sys.databases WHERE name='Covid19_DataWarehouse')
+-- Drop and recreate the 'Clynical_DataWarehouse' database
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name='Clynical_DataWarehouse')
 BEGIN
-	ALTER DATABASE Covid19_DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE Covid19_DataWarehouse;
+	ALTER DATABASE Clynical_DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE Clynical_DataWarehouse;
 END;
 GO
 
--- Create the 'Covid19_DataWarehouse' database
-CREATE DATABASE Covid19_DataWarehouse;
+-- Create the 'Clynical_DataWarehouse' database
+CREATE DATABASE Clynical_DataWarehouse;
 GO
 
-USE Covid19_DataWarehouse;
+USE Clynical_DataWarehouse;
 GO
 
 -- Create Schemas
