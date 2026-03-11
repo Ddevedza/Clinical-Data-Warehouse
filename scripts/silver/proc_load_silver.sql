61e2c3096e2a769a8bd537a4ea60d684d671918c
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-11 20:01:15 +0100

    Implement data loading for careplans and conditions
    Added data loading procedures for careplans and conditions.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -16,3 +16,52 @@ SELECT
 	TRIM(code) code,
 	description
 FROM bronze.allergies;
+
+GO
+
+TRUNCATE TABLE silver.careplans;
+
+INSERT INTO silver.careplans (
+	id,
+	start,
+	stop,
+	patient_id,
+	encounter_id,
+	code,
+	description,
+	reasoncode,
+	reasondescription
+	)
+
+SELECT 
+	TRIM(id) id,
+	TRY_CAST(start as DATE) start, -- Turning data into date if possible
+	TRY_CAST(stop as DATE) stop,
+	TRIM(patient) patient_id,
+	TRIM(encounter) encounter_id,
+	TRIM(code) code,
+	description,
+	TRIM(reasoncode) reasoncode,
+	reasondescription
+FROM bronze.careplans;
+
+GO
+	
+TRUNCATE TABLE silver.conditions;
+
+INSERT INTO silver.conditions (
+	start,
+	stop,
+	patient_id,
+	encounter_id,
+	code,
+	description
+	)
+SELECT
+	TRY_CAST(start as DATE) start, -- Turning data into date if possible
+	TRY_CAST(stop as DATE) stop,
+	TRIM(patient) patient_id,
+	TRIM(encounter) encounter_id,
+	TRIM(code) code,
+	description
+FROM bronze.conditions;
