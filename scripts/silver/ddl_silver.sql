3158e0879f5770084524130f2f495086d769333c
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-14 18:53:36 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -52,16 +52,17 @@ CREATE TABLE silver.conditions( -- then create it
 );
 
 IF OBJECT_ID('silver.devices','U') IS NOT NULL
-	DROP TABLE silver.devices;
-CREATE TABLE silver.devices(
-	start NVARCHAR(50),
-	stop NVARCHAR(50),
-	patient NVARCHAR(255),
-	encounter NVARCHAR(255),
-	code NVARCHAR(50),
-	description NVARCHAR(500),
-	udi NVARCHAR(255),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    DROP TABLE silver.devices; -- drop it
+CREATE TABLE silver.devices( -- then create it
+    -- defining data for each table
+    start DATE,
+    stop DATE,
+    patient_id NVARCHAR(255),
+    encounter_id NVARCHAR(255),
+    code NVARCHAR(50),
+    description NVARCHAR(500),
+    udi NVARCHAR(255),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata when the table was created
 );
 
 IF OBJECT_ID('silver.encounters','U') IS NOT NULL
